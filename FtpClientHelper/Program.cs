3195dc6e800aa8d3dc68038bbdaed9c0f6f3b4e4
CLHuah <huahchunliang@hotmail.com>
2018-09-06 17:11:56 +0800

    FtpClientHelper

---
+++ FtpClientHelper/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using FtpClientHelper.Helper;
+
+namespace FtpClientHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestFtpClientHelper();
+        }
+
+        static void TestFtpClientHelper()
+        {
+            var ftpClientHelper = new FtpClient("ftp://localhost", "testing", "");
+            ftpClientHelper.Download("Download.txt", @"D:/LocalFTP_User/Download_" + Guid.NewGuid() + ".txt");
+            ftpClientHelper.Upload("Upload_" + Guid.NewGuid() + ".txt", @"D:/LocalFTP_User/Upload.txt");
+            ftpClientHelper.Delete("delete.txt");
+            ftpClientHelper.GetFileSize("getFileSize.txt");
+            ftpClientHelper.Rename("oldName.txt", "newName.txt");
+            ftpClientHelper.CreateDirectory("testDir");
+            ftpClientHelper.DeleteDir("testDir");
+            var directoryListDetailed = ftpClientHelper.DirectoryListDetailed("helloworld");
+            var DirectoryListSimple = ftpClientHelper.DirectoryListSimple("helloworld");
+        }
+    }
+}
